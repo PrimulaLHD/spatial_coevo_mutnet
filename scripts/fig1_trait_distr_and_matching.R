3b4af9b11e29232b0b7490080a315a691392aafe
lucaspdmedeiros <lucaspdmedeiros@gmail.com>
2017-09-25 21:45:59 -0300

    add graph and some minor changes

--- scripts/fig1_trait_distr_and_matching.R
+++ scripts/fig1_trait_distr_and_matching.R
@@ -9,6 +9,10 @@
 library(ggplot2)
 library(cowplot)
 library(viridis)
+library(GGally)
+library(network)
+library(sna)
+library(scales)
 
 source("functions/MatchingMutNet.R")
 
@@ -22,6 +26,13 @@ f = rbind(cbind(matrix(0, n_row, n_row), mat),
 
 theta = c(1.5, 9, 5)
 
+# plotting graph
+net = network(f, directed = FALSE, ignore.eval = FALSE)
+
+set.seed(18)
+p_net = ggnet2(net, node.size = 4, color = c("gray80", "gray20", "gray50"), shape = c(15, 15, 16),
+               edge.color = c("black", "black"), edge.size = c(0.8, 0.8), mode = "spring")
+
 #-----------------------------------------------------#
 # Fig. 1A: intermediate trait matching - no gene flow #
 #-----------------------------------------------------#
@@ -31,23 +42,23 @@ z = c(3.7, 6, 4.7)
 # trait distributions
 p_A_1 = ggplot(data = data.frame(x = c(-3, 3)), aes(x)) +
   stat_function(fun = dnorm, args = list(mean = z[1], sd = 1.2),
-                color = "gray80", size = 1) +
+                color = "gray80", size = 1.2) +
   stat_function(fun = dnorm, args = list(mean = z[2], sd = 1.2),
-                color = "black", size = 1) +
+                color = "gray20", size = 1.2) +
   stat_function(fun = dnorm, args = list(mean = z[3], sd = 1.2),
-                color = "gray50", size = 1) +
-  geom_vline(xintercept = theta[1], color = "gray80", size = 0.8,
+                color = "gray50", size = 1.2) +
+  geom_vline(xintercept = theta[1], color = "gray80", size = 0.9,
              linetype = "dashed") +
-  geom_vline(xintercept = theta[2], color = "black", size = 0.8,
+  geom_vline(xintercept = theta[2], color = "gray20", size = 0.9,
              linetype = "dashed") +
-  geom_vline(xintercept = theta[3], color = "gray50", size = 0.8,
+  geom_vline(xintercept = theta[3], color = "gray50", size = 0.9,
              linetype = "dashed") +
   scale_x_continuous(limits = c(0, 17)) +
   xlab("Trait value") +
   ylab("Density") +
   theme(axis.text.x = element_text(size = 13),
         axis.text.y = element_text(size = 13),
-        axis.title = element_text(size = 17),
+        axis.title = element_text(size = 18),
         legend.position = "none")
 
 # trait matching heatmap
@@ -58,12 +69,13 @@ p_A_2 = ggplot(data = matching[[2]], aes(x = column, y = row, fill = matching,
                                 height = 0.95, width = 0.9)) + 
   scale_fill_viridis(option = "plasma", direction = -1,
                      limits = c(0.15, 1), name = "Trait\nmatching") +
-  geom_tile(color = "black", size = 0.6) +
-  xlab("") +
-  ylab("") +
-  theme(axis.text.x = element_text(size = 13),
-        axis.text.y = element_text(size = 13),
-        axis.title = element_text(size = 17),
+  geom_tile(color = "black", size = 0.5) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title = element_blank(),
         axis.ticks = element_blank(),
         legend.position = "none")
   
@@ -76,23 +88,23 @@ z = c(8, 13, 10)
 # trait distributions
 p_B_1 = ggplot(data = data.frame(x = c(-3, 3)), aes(x)) +
   stat_function(fun = dnorm, args = list(mean = z[1], sd = 1.2),
-                color = "gray80", size = 1) +
+                color = "gray80", size = 1.2) +
   stat_function(fun = dnorm, args = list(mean = z[2], sd = 1.2),
-                color = "black", size = 1) +
+                color = "gray20", size = 1.2) +
   stat_function(fun = dnorm, args = list(mean = z[3], sd = 1.2),
-                color = "gray50", size = 1) +
-  geom_vline(xintercept = theta[1], color = "gray80", size = 0.8,
+                color = "gray50", size = 1.2) +
+  geom_vline(xintercept = theta[1], color = "gray80", size = 0.9,
              linetype = "dashed") +
-  geom_vline(xintercept = theta[2], color = "black", size = 0.8,
+  geom_vline(xintercept = theta[2], color = "gray20", size = 0.9,
              linetype = "dashed") +
-  geom_vline(xintercept = theta[3], color = "gray50", size = 0.8,
+  geom_vline(xintercept = theta[3], color = "gray50", size = 0.9,
              linetype = "dashed") +
   scale_x_continuous(limits = c(0, 17)) +
   xlab("Trait value") +
   ylab("Density") +
   theme(axis.text.x = element_text(size = 13),
         axis.text.y = element_text(size = 13),
-        axis.title = element_text(size = 17),
+        axis.title = element_text(size = 18),
         legend.position = "none")
 
 # trait matching heatmap
@@ -103,12 +115,13 @@ p_B_2 = ggplot(data = matching[[2]], aes(x = column, y = row, fill = matching,
                                          height = 0.95, width = 0.9)) + 
   scale_fill_viridis(option = "plasma", direction = -1,
                      limits = c(0.15, 1), name = "Trait\nmatching") +
-  geom_tile(color = "black", size = 0.6) +
-  xlab("") +
-  ylab("") +
-  theme(axis.text.x = element_text(size = 13),
-        axis.text.y = element_text(size = 13),
-        axis.title = element_text(size = 17),
+  geom_tile(color = "black", size = 0.5) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title = element_blank(),
         axis.ticks = element_blank(),
         legend.position = "none")
 
@@ -121,23 +134,23 @@ z = c(9.8, 10.5, 10)
 # trait distributions
 p_C_1 = ggplot(data = data.frame(x = c(-3, 3)), aes(x)) +
   stat_function(fun = dnorm, args = list(mean = z[1], sd = 1.2),
-                color = "gray80", size = 1) +
+                color = "gray80", size = 1.2) +
   stat_function(fun = dnorm, args = list(mean = z[2], sd = 1.2),
-                color = "black", size = 1) +
+                color = "gray20", size = 1.2) +
   stat_function(fun = dnorm, args = list(mean = z[3], sd = 1.2),
-                color = "gray50", size = 1) +
-  geom_vline(xintercept = theta[1], color = "gray80", size = 0.8,
+                color = "gray50", size = 1.2) +
+  geom_vline(xintercept = theta[1], color = "gray80", size = 0.9,
              linetype = "dashed") +
-  geom_vline(xintercept = theta[2], color = "black", size = 0.8,
+  geom_vline(xintercept = theta[2], color = "gray20", size = 0.9,
              linetype = "dashed") +
-  geom_vline(xintercept = theta[3], color = "gray50", size = 0.8,
+  geom_vline(xintercept = theta[3], color = "gray50", size = 0.9,
              linetype = "dashed") +
   scale_x_continuous(limits = c(0, 17)) +
   xlab("Trait value") +
   ylab("Density") +
   theme(axis.text.x = element_text(size = 13),
         axis.text.y = element_text(size = 13),
-        axis.title = element_text(size = 17),
+        axis.title = element_text(size = 18),
         legend.position = "none")
 
 # trait matching heatmap
@@ -148,12 +161,13 @@ p_C_2 = ggplot(data = matching[[2]], aes(x = column, y = row, fill = matching,
                                          height = 0.95, width = 0.9)) + 
   scale_fill_viridis(option = "plasma", direction = -1,
                      limits = c(0.15, 1), name = "Trait\nmatching") +
-  geom_tile(color = "black", size = 0.7) +
-  xlab("") +
-  ylab("") +
-  theme(axis.text.x = element_text(size = 13),
-        axis.text.y = element_text(size = 13),
-        axis.title = element_text(size = 17),
+  geom_tile(color = "black", size = 0.5) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title = element_blank(),
         axis.ticks = element_blank(),
         legend.position = "none")
 
@@ -161,10 +175,8 @@ p_C_2 = ggplot(data = matching[[2]], aes(x = column, y = row, fill = matching,
 p_legend = ggplot(data = matching[[2]], aes(x = column, y = row, fill = matching,
                                          height = 0.95, width = 0.9)) + 
   scale_fill_viridis(option = "plasma", direction = -1,
-                     limits = c(0.15, 1), name = "trait\nmatching") +
+                     limits = c(0.15, 1), name = "Trait\nmatching") +
   geom_tile(color = "black", size = 0.6) +
-  xlab("") +
-  ylab("") +
   theme(legend.title = element_text(size = 15),
         legend.text = element_text(size = 13),
         legend.key.size = unit(0.55, "cm"))
@@ -174,11 +186,14 @@ legend = get_legend(p_legend)
 # generating the whole figure
 fig1 = ggdraw() +
   draw_plot(p_A_1, 0, 0.66, 0.8, 0.22) +
-  draw_plot(p_A_2, 0.57, 0.82, 0.2, 0.17) +
+  draw_plot(p_A_2, 0.53, 0.88, 0.1, 0.1) +
+  draw_plot(p_net, 0.22, 0.89, 0.17, 0.08) +
   draw_plot(p_B_1, 0, 0.33, 0.8, 0.22) +
-  draw_plot(p_B_2, 0.57, 0.49, 0.2, 0.17) +
+  draw_plot(p_B_2, 0.53, 0.55, 0.1, 0.1) +
+  draw_plot(p_net, 0.22, 0.56, 0.17, 0.08) +
   draw_plot(p_C_1, 0, 0, 0.8, 0.22) +
-  draw_plot(p_C_2, 0.57, 0.16, 0.2, 0.17) +
+  draw_plot(p_C_2, 0.53, 0.22, 0.1, 0.1) +
+  draw_plot(p_net, 0.22, 0.23, 0.17, 0.08) +
   draw_plot(legend, 0.8, 0.4, 0.25, 0.15) +
   draw_plot_label(c("A", "B", "C"), c(0, 0, 0), c(0.98, 0.65, 0.32), size = 24)
 
