c9427e0927ed934913060ce6bda1095db0f9c3a8
lucaspdmedeiros <lucaspdmedeiros@gmail.com>
2017-11-24 17:09:44 -0200

    several changes

--- scripts/sim_T_matrix_g_perturb_all_quant_empirical_nets.R
+++ scripts/sim_T_matrix_g_perturb_all_quant_empirical_nets.R
@@ -15,17 +15,19 @@ net_files = dir("data/empirical_networks/weighted/")
 net_names = substr(net_files, start = 1, stop = nchar(net_files) - 4) 
 
 # defining folder to store results
-folder = "~/LUCAS/spatial_coevo_mutnet_results/data/simulations_geneflow_removal"
+folder = "~/Desktop/Lucas_updated/output/data/simulations_geneflow_removal"
 
 # define m_A and m_B sequences
-mA_seq = c(0.1, 0.3, 0.3, 0.5, 0.5, 0.5, 0.7, 0.7, 0.7, 0.7,
-           0.9, 0.9, 0.9, 0.9, 0.9)
-mB_seq = c(0.1, 0.1, 0.3, 0.1, 0.3, 0.5, 0.1, 0.3, 0.5, 0.7,
-           0.1, 0.3, 0.5, 0.7, 0.9)
-# define gene flow value
-g = 0.3
+m_df = expand.grid(seq(0.1, 0.9, 0.2), seq(0.1, 0.9, 0.2))
+m_df = m_df[m_df[ , 1] >= m_df[ , 2], ]
+names(m_df) = c("mA", "mB")
+
+# define gene flow values
+g_high = 0.3
+g_low = 0
 # for the file name
-g_char = gsub(".", "", as.character(g), fixed = TRUE)
+g_high_char = gsub(".", "", as.character(g_high), fixed = TRUE)
+g_low_char = gsub(".", "", as.character(g_low), fixed = TRUE)
 # define perturbation sequence
 pert_g_seq = seq(0, 1, by = 0.05)
 # defining theta distribution
@@ -34,9 +36,9 @@ theta_A_max = 10
 theta_B_min = 10
 theta_B_max = 20
 # number of theta vectors 
-n_theta = 30
+n_theta = 10
 # number of replicates of perturbation simulations
-n_rep_pert = 30
+n_rep_pert = 10
 
 for(i in 1:length(net_files)) {
   
@@ -49,20 +51,23 @@ for(i in 1:length(net_files)) {
   n_col = ncol(mat)
   n_sp = n_row + n_col
   
-  results_df = data.frame(network = rep(net_names[i], times = n_rep_pert*n_theta*2*length(mA_seq)*length(pert_g_seq)),
-                          rep_pert = rep(rep(1:n_rep_pert, each = length(pert_g_seq)*n_theta*2), length(mA_seq)),
-                          rep_theta = rep(rep((1:n_theta), each = 2), times = n_rep_pert*length(mA_seq)*length(pert_g_seq)),
-                          site = rep(rep(c("A", "B"), times = n_theta), times = n_rep_pert*length(mA_seq)*length(pert_g_seq)),
-                          pert = rep(rep(pert_g_seq, each = n_theta*2), times = n_rep_pert*length(mA_seq)),
-                          m_A = rep(rep(mA_seq, each = length(pert_g_seq)), each = n_rep_pert*n_theta*2),
-                          m_B = rep(rep(mB_seq, each = length(pert_g_seq)), each = n_rep_pert*n_theta*2),
-                          matching = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2),
-                          mean_eigen = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2),
-                          sd_eigen = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2),
-                          eigen1 = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2),
-                          eigen2 = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2),
-                          det_T = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2),
-                          mean_col_sd = rep(NA, times = length(mA_seq)*length(pert_g_seq)*n_rep_pert*n_theta*2))
+  results_df = data.frame(network = rep(net_names[i], times = n_rep_pert*n_theta*nrow(m_df)*length(pert_g_seq)),
+                          rep_pert = rep(rep(1:n_rep_pert, each = length(pert_g_seq)*n_theta), nrow(m_df)),
+                          rep_theta = rep(1:n_theta, times = n_rep_pert*nrow(m_df)*length(pert_g_seq)),
+                          pert = rep(rep(pert_g_seq, each = n_theta), times = n_rep_pert*nrow(m_df)),
+                          m_A = rep(rep(m_df$mA, each = length(pert_g_seq)), each = n_rep_pert*n_theta),
+                          m_B = rep(rep(m_df$mB, each = length(pert_g_seq)), each = n_rep_pert*n_theta),
+                          matching_A = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          matching_B = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          dim_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          sum_eigen_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          mean_eigen_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          sd_eigen_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          eigen1_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          eigen2_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          det_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          mean_row_corr_T = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta),
+                          sd_g = rep(NA, times = nrow(m_df)*length(pert_g_seq)*n_rep_pert*n_theta))
 
   # Q and F matrices
   Q = rbind(cbind(matrix(0, n_row, n_row), mat),
@@ -76,50 +81,60 @@ for(i in 1:length(net_files)) {
   # identity matrix
   I = diag(rep(1, 2*n_sp))
   
-  for (j in 1:length(mA_seq)) {
+  for (j in 1:nrow(m_df)) {
     
     # matrix M
-    m_A = rep(mA_seq[j], n_sp)
-    m_B = rep(mB_seq[j], n_sp)
+    m_A = rep(m_df$mA[j], n_sp)
+    m_B = rep(m_df$mB[j], n_sp)
     M = diag(c(m_A, m_B))
     
-    print(mA_seq[j])
-    print(mB_seq[j])
+    print(m_df$mA[j])
+    print(m_df$mB[j])
     
     for (k in 1:n_rep_pert) {
-      
-      # matrix G
-      g_mult = rep(g, n_sp)
-      G = rbind(cbind(diag(1 - g_mult), diag(g_mult)),
-                cbind(diag(g_mult), diag(1 - g_mult)))
       
       # sampling species perturbation order
       sp_order = sample(1:n_sp, replace = FALSE)
       
       for (l in 1:length(pert_g_seq)) {
+        # number of species that will loose gene flow
+        sp_number = round(pert_g_seq[l]*n_sp)
+        # creating gene flow vector 
+        g_mult = rep(g_high, n_sp)
         
-        # perturbing matrix G
-        sp_index = round(pert_g_seq[l]*n_sp)
-        if (sp_index != 0) {
-          pert_sp = sp_order[1:sp_index]
-          G[c(pert_sp, pert_sp + n_sp), c(pert_sp, pert_sp + n_sp)] = diag(1, length(pert_sp)*2) 
+        if (sp_number != 0) {
+          # indexes of species that will loose gene flow
+          pert_sp = sp_order[1:sp_number]
+          # modifying gene flow vector
+          g_mult[pert_sp] = g_low
         }
+        # creating G matrix
+        G = rbind(cbind(diag(1 - g_mult), diag(g_mult)),
+                  cbind(diag(g_mult), diag(1 - g_mult)))
         
-        # build T matrix
+        # standard deviation of gene flow values
+        sd_g = sd(g_mult)
+        
+        # T matrix
         T_matrix = solve(solve(G) -  M %*% Q_2sites) %*% (I - M)
+        # dimension T
+        dim_T = nrow(T_matrix)
         # T matrix eigenvalues
-        eigens = Re(eigen(T_matrix)$values)
+        eigen_T = Re(eigen(T_matrix)$values)
+        # sum of eigenvalues
+        sum_eigen_T = sum(eigen_T)
         # mean of eigenvalues
-        mean_eigens = mean(eigens)
+        mean_eigen_T = mean(eigen_T)
         # sd of eigenvalues
-        sd_eigens = sd(eigens)
+        sd_eigen_T = sd(eigen_T)
         # 1st and 2nd eigenvalues
-        eigen1 = eigens[1]
-        eigen2 = eigens[2]
+        eigen1_T = eigen_T[1]
+        eigen2_T = eigen_T[2]
         # determinant
-        det_T = det(T_matrix)
-        # mean column sd
-        mean_col_sd = mean(apply(T_matrix, 2, sd))
+        det_T = prod(eigen_T)
+        # mean correlation among rows
+        corr_mat = T_matrix%*%t(T_matrix)
+        mean_row_corr_T = mean(corr_mat)
         
         for (t in 1:n_theta) {
           
@@ -141,33 +156,49 @@ for(i in 1:length(net_files)) {
           mean_mat_B = match_B[[1]]
           
           # adding results to data frame
-          results_df$matching[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                                results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                                results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(mean_mat_A, mean_mat_B)
+          results_df$matching_A[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                  results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                  results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = mean_mat_A
           
-          results_df$mean_eigen[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                                  results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                                  results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(mean_eigens, mean_eigens)
+          results_df$matching_B[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                  results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                  results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = mean_mat_B
           
-          results_df$sd_eigen[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                                results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                                results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(sd_eigens, sd_eigens)
+          results_df$dim_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                             results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                             results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = dim_T
           
-          results_df$eigen1[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                              results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                              results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(eigen1, eigen1)
+          results_df$sum_eigen_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                   results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                   results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = sum_eigen_T
           
-          results_df$eigen2[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                              results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                              results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(eigen2, eigen2)
+          results_df$mean_eigen_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                    results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                    results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = mean_eigen_T
           
-          results_df$det_T[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                             results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                             results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(det_T, det_T)
+          results_df$sd_eigen_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                  results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                  results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = sd_eigen_T
           
-          results_df$mean_col_sd[results_df$network ==  net_names[i] & results_df$m_A ==  mA_seq[j] & 
-                                   results_df$m_B ==  mB_seq[j] & results_df$rep_pert == k & 
-                                   results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = c(mean_col_sd, mean_col_sd)
+          results_df$eigen1_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = eigen1_T
+          
+          results_df$eigen2_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = eigen2_T
+          
+          results_df$det_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                             results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                             results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = det_T
+          
+          results_df$mean_row_corr_T[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                                     results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                                     results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = mean_row_corr_T
+          
+          results_df$sd_g[results_df$network ==  net_names[i] & results_df$m_A ==  m_df$mA[j] & 
+                            results_df$m_B ==  m_df$mB[j] & results_df$rep_pert == k & 
+                            results_df$pert == pert_g_seq[l] & results_df$rep_theta == t] = sd_g
            
         }
       }
@@ -175,8 +206,8 @@ for(i in 1:length(net_files)) {
   }
   
   # saving results
-  write.csv(results_df, file = paste(folder, "/", net_names[i], "_geneflow_removal_from_g", 
-                                     g_char, ".csv", sep = ""), row.names = FALSE)
+  write.csv(results_df, file = paste(folder, "/", net_names[i], "_geneflow_removal_g", 
+                                     g_high_char, "-", g_low_char, ".csv", sep = ""), row.names = FALSE)
   
 }
 
