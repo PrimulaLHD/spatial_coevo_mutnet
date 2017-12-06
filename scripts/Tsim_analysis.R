2c3239ceca82d366ed4b8053ae4dc19b23bd975b
Guilherme Garcia <wgar@usp.br>
2017-12-06 16:27:14 -0200

    figs S5/S8

--- scripts/Tsim_analysis.R
+++ scripts/Tsim_analysis.R
@@ -52,39 +52,153 @@ gzero.sumdf <-
     summarise_each(funs(mean))
 
 
-## FIG S5
+## FIG gzero_all_sites_trait_match
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_gzero_all_sites_trait_match_vs_corr.pdf', width = 8, height = 5)
+
 ggplot(gzero.sumdf) +
     geom_line(aes(x = final.mcorr.lines, y = final.tm, group = network),
               alpha = 0.5) +
     geom_point(aes(x = final.mcorr.lines, y = final.tm, color = mA)) +
-    facet_wrap(~ type) +
+    #facet_wrap(~ type) +
     #facet_grid(mA ~ mB) +
-    scale_color_viridis(option = 'C', direction = -1) +
+    scale_color_viridis('Mutualism\nStrength', option = 'C', direction = -1) +
     theme_bw() +
-    #theme(legend.position = 'none') +
-    scale_y_log10()
+    ylab('Trait Match') +
+    xlab('Landscape Average Correlation')
 
-plot_grid(
-    ggplot(subset(T.sims.sumdf, mA == '0.7' & mB == '0.7')) +
+dev.off(dev.cur())
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_gzero_all_sites_trait_match_vs_sd.pdf',
+    width = 8, height = 5)
+
+ggplot(gzero.sumdf) +
+    geom_line(aes(x = final.sd.lines, y = final.tm, group = network),
+              alpha = 0.5) +
+    geom_point(aes(x = final.sd.lines, y = final.tm, color = mA)) +
+    scale_color_viridis('Mutualism\nStrength', option = 'C', direction = -1) +
+    theme_bw() +
+    ylab('Trait Match') +
+    xlab('Landscape Average Standard Deviation')
+
+dev.off(dev.cur())
+
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_gzero_all_sites_trait_match_vs_perc.pdf', width = 8, height = 5)
+
+ggplot(gzero.sumdf) +
+    geom_line(aes(x = final.perc, y = final.tm, group = network),
+              alpha = 0.5) +
+    geom_point(aes(x = final.perc, y = final.tm, color = mA)) +
+    scale_color_viridis('Mutualism\nStrength', option = 'C', direction = -1) +
+    theme_bw() +
+    ylab('Trait Match') +
+    xlab('% First Eigenvalue')
+
+dev.off(dev.cur())
+
+## FIG S8
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_sitetype_m07_trait_match_vs_corr.pdf',
+    width = 8, height = 8)
+
+ggplot(subset(T.sims.sumdf, mA == '0.7' & mB == '0.7')) +
     geom_line(aes(x = final.mcorr.lines, y = final.tm, group = network),
               alpha = 0.5) +
     geom_point(aes(x = final.mcorr.lines, y = final.tm, color = g), alpha = 0.5) +    
     facet_wrap(~ type, nrow = 3) +
     ##facet_grid(mA ~ mB) +
     scale_color_viridis(name = 'Gene\nFlow', option = 'A', direction = -1) +
-    ##scale_y_log10() +
-    scale_x_log10() +
-    theme_bw(), 
+    ggtitle('mA = 0.7, mB = 0.7') +
+    xlab('Average Landscape Correlation') +
+    ylab('Trait Match') +
+    theme_bw() 
 
-    ggplot(subset(T.sims.sumdf, mA == '0.9' & mB == '0.1')) +
+dev.off(dev.cur())
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_sitetype_m07_trait_match_vs_sd.pdf',
+    width = 8, height = 8)
+
+ggplot(subset(T.sims.sumdf, mA == '0.7' & mB == '0.7')) +
+    geom_line(aes(x = final.sd.lines, y = final.tm, group = network),
+              alpha = 0.5) +
+    geom_point(aes(x = final.sd.lines, y = final.tm, color = g), alpha = 0.5) +    
+    facet_wrap(~ type, nrow = 3) +
+    ##facet_grid(mA ~ mB) +
+    scale_color_viridis(name = 'Gene\nFlow', option = 'A', direction = -1) +
+    ggtitle('mA = 0.7, mB = 0.7') +
+    xlab('Average Landscape Standard Deviation') +
+    ylab('Trait Match') +
+    theme_bw() 
+
+dev.off(dev.cur())
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_sitetype_m07_trait_match_vs_perc.pdf',
+    width = 8, height = 8)
+
+ggplot(subset(T.sims.sumdf, mA == '0.7' & mB == '0.7')) +
+    geom_line(aes(x = final.perc, y = final.tm, group = network),
+              alpha = 0.5) +
+    geom_point(aes(x = final.perc, y = final.tm, color = g), alpha = 0.5) +    
+    facet_wrap(~ type, nrow = 3) +
+    ##facet_grid(mA ~ mB) +
+    scale_color_viridis(name = 'Gene\nFlow', option = 'A', direction = -1) +
+    ggtitle('mA = 0.7, mB = 0.7') +
+    xlab('% First Eigenvalue') +
+    ylab('Trait Match') +
+    theme_bw() 
+
+dev.off(dev.cur())
+
+## FIG S8_2
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_sitetype_mA09_mB01_trait_match_vs_corr.pdf',
+    width = 8, height = 8)
+
+ggplot(subset(T.sims.sumdf, mA == '0.9' & mB == '0.1')) +
     geom_line(aes(x = final.mcorr.lines, y = final.tm, group = network),
               alpha = 0.5) +
     geom_point(aes(x = final.mcorr.lines, y = final.tm, color = g), alpha = 0.5) +    
     facet_wrap(~ type, nrow = 3) +
     ##facet_grid(mA ~ mB) +
     scale_color_viridis(name = 'Gene\nFlow', option = 'A', direction = -1) +
-    ##scale_y_log10() +
-    scale_x_log10() +
-    theme_bw(), 
+    ggtitle('mA = 0.7, mB = 0.7') +
+    xlab('Average Landscape Correlation') +
+    ylab('Trait Match') +
+    theme_bw() 
 
-    ncol = 2)
+dev.off(dev.cur())
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_sitetype_mA09_mB01_trait_match_vs_sd.pdf',
+    width = 8, height = 8)
+
+ggplot(subset(T.sims.sumdf, mA == '0.9' & mB == '0.1')) +
+    geom_line(aes(x = final.sd.lines, y = final.tm, group = network),
+              alpha = 0.5) +
+    geom_point(aes(x = final.sd.lines, y = final.tm, color = g), alpha = 0.5) +    
+    facet_wrap(~ type, nrow = 3) +
+    ##facet_grid(mA ~ mB) +
+    scale_color_viridis(name = 'Gene\nFlow', option = 'A', direction = -1) +
+    ggtitle('mA = 0.7, mB = 0.7') +
+    xlab('Average Landscape Standard Deviation') +
+    ylab('Trait Match') +
+    theme_bw() 
+
+dev.off(dev.cur())
+
+pdf('~/Dropbox/spatial_coevo_mutnet/results/figs/figSI_sitetype_mA09_mB01_trait_match_vs_perc.pdf',
+    width = 8, height = 8)
+
+ggplot(subset(T.sims.sumdf, mA == '0.9' & mB == '0.1')) +
+    geom_line(aes(x = final.perc, y = final.tm, group = network),
+              alpha = 0.5) +
+    geom_point(aes(x = final.perc, y = final.tm, color = g), alpha = 0.5) +    
+    facet_wrap(~ type, nrow = 3) +
+    ##facet_grid(mA ~ mB) +
+    scale_color_viridis(name = 'Gene\nFlow', option = 'A', direction = -1) +
+    ggtitle('mA = 0.7, mB = 0.7') +
+    xlab('% First Eigenvalue') +
+    ylab('Trait Match') +
+    theme_bw() 
+
+dev.off(dev.cur())
+
