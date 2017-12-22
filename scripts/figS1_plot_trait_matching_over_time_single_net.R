8769dff4844941bebf29dbf5b3d793a5c51185df
lucaspdmedeiros <lucaspdmedeiros@gmail.com>
2017-12-22 17:41:32 -0200

    add scripts for figs S1 and S2

---
+++ scripts/figS1_plot_trait_matching_over_time_single_net.R
@@ -0,0 +1,107 @@
+#-----------------------------------------------------------------------------------------------------#
+
+# Description: 
+#   Reads trait evolution through time data for one network and plots trait matching as a function of
+#   time for different values of mutualistic selection.
+#
+# Returns:
+#   A plot.
+
+# loading functions and packages
+library(ggplot2)
+library(cowplot)
+library(viridis)
+source("functions/MatchingMutNet.R")
+
+# defining network
+network = "FP_Galetti&Pizo_1996_bin"
+
+# reading network
+mat = as.matrix(read.table(paste("data/empirical_networks/binary/", network, ".txt", sep = ""), 
+                           sep = " ", header = FALSE))
+# defining number of rows, columns and species
+n_row = nrow(mat)
+n_col = ncol(mat)
+n_sp = n_row + n_col
+# building the square adjacency matrix f
+f = rbind(cbind(matrix(0, n_row, n_row), mat), 
+          cbind(t(mat), matrix(0, n_col, n_col)))
+
+# defining m values
+mA = c(0.1, 0.3, 0.5, 0.7, 0.9)
+mB = 0.1
+# defining mutualistic selection scenario
+mA_char = paste("mA", gsub(".", "", mA, fixed = TRUE), sep = "")
+mB_char = paste("mB", gsub(".", "", mB, fixed = TRUE), sep = "")
+m_char = paste(mA_char, mB_char, sep = "_")
+# defining gene flow value
+g = 0
+g_char = paste("g", g, "_", sep = "")
+# defining site
+site_char = "siteA"
+
+# alpha value
+alpha = 0.2
+
+# vector to store mutualistic selection
+m = c()
+# vector to store times to equilibrium
+time = c()
+# vector to store trait matching through time
+mean_mut_mat = c()
+# vector to store simulation number
+simulation = c()
+
+for (i in 1:length(m_char)) {
+  # obtaining simulation files
+  folders = dir("~/LUCAS/spatial_coevo_mutnet_results/data/simulations_empirical_networks/simulations_all_timesteps/")
+  folder = folders[grep(m_char[i], folders)]
+  files = dir(paste("~/LUCAS/spatial_coevo_mutnet_results/data/simulations_empirical_networks/simulations_all_timesteps/",
+                    folder, "/", network, sep = ""))
+  files_g = files[grep(g_char, files)]
+  files_g_site = files_g[grep(site_char, files_g)]
+  
+  for (j in 1:length(files_g_site)) {
+    # read trait values through time
+    traits_df = read.csv(paste("~/LUCAS/spatial_coevo_mutnet_results/data/simulations_empirical_networks/simulations_all_timesteps/",
+                               folder, "/", network, "/", files_g_site[j], sep = ""))
+    # removing 1st column with labels
+    traits_df = traits_df[ , 2:ncol(traits_df)]
+    # theta values
+    thetas = traits_df[1, ]
+    # removing theta values
+    traits_df = traits_df[2:nrow(traits_df), ]
+    # time to equilibrium
+    time = c(time, 1:nrow(traits_df))
+    for (k in 1:nrow(traits_df)) {
+      # traits at time j
+      traits = as.numeric(traits_df[k, ])
+      # trait matching
+      mut_mat = MatchingMutNet(n_sp = n_sp, n_row = n_row, n_col = n_col, f = f,
+                               z = traits, method = "exponential", alpha = alpha)
+      mean_mut_mat = c(mean_mut_mat, mut_mat[[1]])
+    }
+    
+    simulation = c(simulation, rep(j, times = nrow(traits_df)))
+    m = c(m, rep(mA[i], times = nrow(traits_df)))
+  
+  }
+  
+  results_df = data.frame(m = m,
+                          simulation = simulation,
+                          time = time,
+                          mean_mut_mat = mean_mut_mat)
+  
+}
+
+ggplot(data = subset(results_df, time < 30), 
+       mapping = aes(x = time, y = mean_mut_mat, group = simulation)) +
+  geom_line(size = 0.8, color = "black", alpha = 0.5) +
+  xlab("Time") +
+  ylab("Mean trait matching") +
+  facet_grid( ~ m, labeller = label_both, scales = "free") +
+  theme(axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),
+        axis.title = element_text(size = 14))
+
+#-----------------------------------------------------------------------------------------------------#
