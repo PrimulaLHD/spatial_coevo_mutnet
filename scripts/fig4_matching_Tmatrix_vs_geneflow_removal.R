4d627473b4a3ade38cc9599e760f1baa38fbb2d8
lucaspdmedeiros <lucaspdmedeiros@gmail.com>
2018-04-12 09:36:26 -0300

    organize code for fig 4

--- scripts/fig4_matching_Tmatrix_vs_geneflow_removal.R
+++ scripts/fig4_matching_Tmatrix_vs_geneflow_removal.R
@@ -1,18 +1,20 @@
 #-----------------------------------------------------------------------------------------------------#
 
 # Description: 
-#   (Organize this code). Code for figure 4: gene flow perturbation, T matrix and trait matching
+#
 #
 # Returns:
 #   
 
+# loading functions and packages
 library(plyr)
 library(ggplot2)
 library(cowplot)
 
-net_struct = read.csv("output/data/network_structure/network_structure.csv")
+# defining folder with results
 result_files = dir("~/LUCAS/spatial_coevo_mutnet_results/data/simulations_empirical_networks/simulations_geneflow_removal/")
 
+# selecting the files with the desired gene flow values
 result_files = result_files[grep("g03-0.csv", result_files)]
 # result_files = result_files[grep("g03-001.csv", result_files)]
 # result_files = result_files[grep("g03-005.csv", result_files)]
@@ -21,9 +23,11 @@ result_files = result_files[grep("g03-0.csv", result_files)]
 # result_files = result_files[grep("g01-001.csv", result_files)]
 # result_files = result_files[grep("g01-005.csv", result_files)]
 
+# getting network names
 net_names = gsub( "_geneflow.*$", "", result_files)
 net_names_bin = gsub("wgt", "bin", net_names)
 
+# summarizing simulation results
 summ_df = c()
 
 for (i in 1:length(result_files)) {
@@ -36,59 +40,49 @@ for (i in 1:length(result_files)) {
                        matching_A = mean(matching_A),
                        matching_B = mean(matching_B),
                        dim_T = unique(dim_T),
-                       eigen1_L = mean(eigen1_L),
-                       eigen2_L = mean(eigen2_L),
                        sum_eigen_T = mean(sum_eigen_T),
                        mean_eigen_T = mean(mean_eigen_T),
                        sd_eigen_T = mean(sd_eigen_T),
                        eigen1_T = mean(eigen1_T),
                        eigen2_T = mean(eigen2_T),
                        det_T = mean(det_T),
-                       mean_col_sd_T = mean(mean_col_sd_T))
+                       mean_row_corr_T = mean(mean_row_corr_T),
+                       sd_g = mean(sd_g))
   
   summ_df = rbind(summ_df, curr_summ_df)
 }
 
+# magnitude of 1st eigenvalue in relation to all others
 summ_df$prop_eigen1_T = summ_df$eigen1_T/summ_df$sum_eigen_T
 
+# obtaining mutualism names
 summ_df$mutualism = substr(summ_df$network, start = 1, stop = 2) 
 
 # changing mutualism names
-summ_df$mutualism[summ_df$mutualism == "AE"] = "ant-nectary bearing plant"
-summ_df$mutualism[summ_df$mutualism == "AM"] = "ant-myrmecophyte"
+summ_df$mutualism[summ_df$mutualism == "AE"] = "ants - nectary-bearing plants"
+summ_df$mutualism[summ_df$mutualism == "AM"] = "ants - myrmecophytes"
 summ_df$mutualism[summ_df$mutualism == "FP"] = "seed dispersal"
 summ_df$mutualism[summ_df$mutualism == "PP"] = "pollination"
 
 # defining a color palette and ordering according to factor mutualism
-my_palette = c("#CD2626", "#00EEEE", "#A020F0", "#1AB245")
+my_palette = c("#CD2626", "#00EEEE", "#1AB245", "#A020F0")
 
-ggplot(data = subset(summ_df, m_A == 0.5 & m_B == 0.5), 
-       aes(x = pert, y = matching_A, group = network, color = mutualism)) +
-  geom_point(size = 2.4) +
-  geom_line(size = 0.5) +
+# plotting
+fig4 = ggplot(data = subset(summ_df, m_A == 0.5 & m_B == 0.5), 
+           aes(x = pert, y = matching_A, group = network, color = mutualism)) +
+  geom_point(size = 2) +
+  geom_line(size = 0.4) +
   scale_color_manual(values = my_palette) +
   xlab("Fraction of species without gene flow") +
   ylab("Mean trait matching") +
-  theme(axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 14),
-        axis.title = element_text(size = 16),
-        legend.key.size = unit(0.6, "cm"),
-        legend.text = element_text(size = 12),
+  theme(axis.text.x = element_text(size = 13),
+        axis.text.y = element_text(size = 13),
+        axis.title = element_text(size = 15),
+        legend.key.size = unit(0.5, "cm"),
+        legend.text = element_text(size = 11),
         legend.title = element_blank())
 
-ggplot(data = summ_df,
-       aes(x = mean_col_sd_T, y = matching_A, color = network)) +
-  geom_line(size = 0.8, alpha = 0.8) +
-  #scale_color_manual(values = my_palette) +
-  xlab("Sd rows of T-matrix") +
-  ylab("Mean trait matching") +
-  facet_grid(m_A ~ m_B, labeller = label_both, scales = "free") +
-  theme(axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 14),
-        axis.title = element_text(size = 16),
-        legend.key.size = unit(0.6, "cm"),
-        legend.text = element_text(size = 12),
-        legend.title = element_blank(),
-        legend.position = "none")
+# saving
+save_plot("fig4.pdf", fig4, ncol = 1, nrow = 1, base_aspect_ratio = 1.8)
 
 #-----------------------------------------------------------------------------------------------------#
